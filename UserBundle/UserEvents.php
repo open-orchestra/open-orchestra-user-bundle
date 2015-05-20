6df8d3ecfe201e12228133c02eff4414068a51f9
romain boessel <romain.boessel@businessecision.com>
2015-05-20 10:29:13 +0200

    override FOSUser password form view and add USER_CHANGE_PASSWORD event

--- UserBundle/UserEvents.php
+++ UserBundle/UserEvents.php
@@ -10,4 +10,5 @@ class UserEvents
     const USER_CREATE = 'user.create';
     const USER_DELETE = 'user.delete';
     const USER_UPDATE = 'user.update';
+    const USER_CHANGE_PASSWORD = "user.change_password";
 }
