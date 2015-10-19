310e7ab1cf89103d88b2d6275d51b49fcddc2990
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-10-19 11:54:23 +0200

    add test di extension

---
+++ UserBundle/Tests/DependencyInjection/OpenOrchestraUserExtensionTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace OpenOrchestra\BaseApiBundle\DependencyInjection;
+
+use OpenOrchestra\UserBundle\DependencyInjection\OpenOrchestraUserExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class OpenOrchestraBaseApiExtensionTest
+ */
+class OpenOrchestraUserExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test default value configuration
+     */
+    public function testDefaultConfig()
+    {
+        $container = $this->loadContainerFromFile('empty');
+
+        $this->assertEquals('OpenOrchestraUserBundle::baseLayout.html.twig', $container->getParameter('open_orchestra_user.base_layout'));
+        $this->assertEquals('OpenOrchestraUserBundle::form.html.twig', $container->getParameter('open_orchestra_user.form_template'));
+    }
+
+    /**
+     * Test configuration with value
+     */
+    public function testConfigWithValue()
+    {
+        $container = $this->loadContainerFromFile('value');
+
+        $this->assertEquals('base_layout_fake.html.twig', $container->getParameter('open_orchestra_user.base_layout'));
+        $this->assertEquals('form_template_fake.html.twig', $container->getParameter('open_orchestra_user.form_template'));
+    }
+
+    /**
+     * @param string $file
+     *
+     * @return ContainerBuilder
+     */
+    private function loadContainerFromFile($file)
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter('kernel.debug', false);
+        $container->setParameter('kernel.cache_dir', '/tmp');
+        $container->setParameter('fos_user.model.user.class', 'fake_fos_user.class');
+        $container->registerExtension(new OpenOrchestraUserExtension());
+
+        $locator = new FileLocator(__DIR__ . '/Fixtures/config/');
+        $loader = new YamlFileLoader($container, $locator);
+        $loader->load($file . '.yml');
+        $container->getCompilerPassConfig()->setOptimizationPasses(array());
+        $container->getCompilerPassConfig()->setRemovingPasses(array());
+        $container->compile();
+
+        return $container;
+    }
+}
