d8901890db954dfbe66a9691f414ea30350cfdc4
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-11-09 15:49:14 +0100

    add proper search in repository

--- UserBundle/Repository/UserRepository.php
+++ UserBundle/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace OpenOrchestra\UserBundle\Repository;
 
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
+use FOS\UserBundle\Model\GroupInterface;
 
 /**
  * Class UserRepository
@@ -20,5 +21,15 @@ class UserRepository extends AbstractAggregateRepository
     public function findOneByUsername($username)
     {
         return $this->findOneBy(array('username' => $username));
+    }
+
+    /**
+     * @param string GroupInterface $group
+     *
+     * @return array
+     */
+    public function findByGroup(GroupInterface $group)
+    {
+        return $this->findBy(array('groups.$id' => new \MongoId($group->getId())));
     }
 }
