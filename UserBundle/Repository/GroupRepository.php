39312bea7b031697cde9485d7df4c88262c56e83
Nicolas Thal <nico.th4l@gmail.com>
2015-11-19 10:15:14 +0100

    group and user should be paginable

--- UserBundle/Repository/GroupRepository.php
+++ UserBundle/Repository/GroupRepository.php
@@ -2,13 +2,14 @@
 
 namespace OpenOrchestra\UserBundle\Repository;
 
+use OpenOrchestra\Pagination\Configuration\PaginationRepositoryInterface;
 use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 
 /**
  * Class GroupRepository
  */
-class GroupRepository extends AbstractAggregateRepository
+class GroupRepository extends AbstractAggregateRepository implements PaginationRepositoryInterface
 {
     use PaginationTrait;
 }
