2a3ee95576e018a4ba7c1f583343d8afded4f4bf
itkg-nanne <nicolas.anne@businessdecision.com>
2016-12-06 17:02:24 +0100

    add new pagination and find (#132)

--- UserBundle/Repository/GroupRepository.php
+++ UserBundle/Repository/GroupRepository.php
@@ -2,14 +2,11 @@
 
 namespace OpenOrchestra\UserBundle\Repository;
 
-use OpenOrchestra\Pagination\Configuration\PaginationRepositoryInterface;
-use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 
 /**
  * Class GroupRepository
  */
-class GroupRepository extends AbstractAggregateRepository implements PaginationRepositoryInterface
+class GroupRepository extends AbstractAggregateRepository
 {
-    use PaginationTrait;
 }
